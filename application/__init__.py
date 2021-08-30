104b53c7ad15e6bd7e3a9839d0c913a0863b4c25
Sairaj Saish Mahendra Prabhu Dharwadkar <sdharwadkar@oneshield.com>
2021-08-30 11:35:38 +0530

    time pass

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/next")
-def next():
-    return "hi"
-
+from application import routes
