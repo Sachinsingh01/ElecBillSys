8b9d8c8e129ac5a0ac36ab77d179f8bdaadaa1e4
Sairaj Saish Mahendra Prabhu Dharwadkar <sdharwadkar@oneshield.com>
2021-09-08 12:13:43 +0530

    no chng

--- application/Billing.py
+++ application/Billing.py
@@ -13,6 +13,7 @@ class Bill():
         self.conn = conn
         self.cursor = conn.cursor(pymysql.cursors.DictCursor)
         self.cursor.execute("SELECT Co_Type_ID from connection where Meter_No = %s",(meterNo))
+        self.meterNo = meterNo
         self.connType = self.cursor.fetchone()['Co_Type_ID']
         self.prevDate = self.getDate(prevDate)
         self.currDate = self.getDate(readDate)
@@ -25,29 +26,13 @@ class Bill():
     
     def getAmount(self):
 
-        consumption = self.currReading - self.prevReading
-        self.cursor.execute("SELECT Units_To from slab_charges WHERE From_Date = (SELECT MAX(From_Date) from slab_charges) and S_Charge_Type = %s and Con_Type_ID = %s",("EC",self.connType)) 
-        temp = self.cursor.fetchall()
-        slabs=[]
-        for t in temp:
-            slabs.append(t['Units_To'])
-        print(slabs)
+        pass
 
         #find the days passed between prev and current date
-        billingPeriod = (self.currDate - self.prevDate).days
-        print(billingPeriod)
-        print(str(self.prevDate))
-        self.cursor.execute("SELECT DISTINCT From_Date FROM slab_charges WHERE (From_Date >%s and From_Date <%s) or From_Date =(SELECT MAX(From_Date) from slab_charges where From_Date <=%s) ORDER BY From_Date Desc ",(str(self.prevDate),str(self.currDate),str(self.prevDate)))
-        Temp = self.cursor.fetchall()
-        dates = []
-        for t in Temp:
-            print(t)
-            dates.append(t['From_Date'])
-        #if prev reading date >= last change of slabs 
-            #simple calculations 
-        #else 
-            #go uptill prev date >= last change and calculate for every change
+        
+        
         #fill the required tables  or return amount 
+    
         #do not forget to update the billing calender
 
     def getCurrDate(self):
@@ -86,4 +71,6 @@ class Bill():
         temp = date.split("/")
         dat = f"{temp[2]}-{temp[0]}-{temp[1]}"
         return datetime.strptime(dat,'%Y-%m-%d').date()
+    
+
     
