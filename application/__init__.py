cc0905498ea1df79c0fe0b9fc396bcca5ce200a4
Sairaj Saish Mahendra Prabhu Dharwadkar <sdharwadkar@oneshield.com>
2021-09-09 12:00:00 +0530

    asdfasdf

--- application/__init__.py
+++ application/__init__.py
@@ -268,12 +268,12 @@ def billsList():
 
 @app.route("/billDetail")
 def billDetail():
-    cid = session["id"]
-    conn = mysql.connect()
-    bill = Bill(conn, request,cid)
-    print(bill.getAmount())
+    # cid = session["id"]
+    # conn = mysql.connect()
+    # bill = Bill(conn, request,cid)
+    # print(bill.getAmount())
     # amount = pass
-    js = {"fname":consumer.fname, "lname":consumer.lname, "cid":consumer.cid, "address":consumer.address, "taluka":consumer.taluka, "district":consumer.district, "pinCode":consumer.pinCode, "meterId":consumer.meterId, "conType":consumer.conType, "contact":consumer.contact, "sanctionedLoad":consumer.sanctionedLoad}
+    # js = {"fname":consumer.fname, "lname":consumer.lname, "cid":consumer.cid, "address":consumer.address, "taluka":consumer.taluka, "district":consumer.district, "pinCode":consumer.pinCode, "meterId":consumer.meterId, "conType":consumer.conType, "contact":consumer.contact, "sanctionedLoad":consumer.sanctionedLoad}
     return render_template("billDetail.html" ) 
 
 @app.route("/adminConn", methods=["POST", "GET"])
